a4cd4ed883405edc81cba51a8629dd41240ba78a
() <nilclass@riseup.net>
2013-04-29 16:52:28 +0200

    rewrote indexedDB backend - NOT WORKING, BECAUSE: * indexedDB's transactions suck (no offense indexedDB) * some errors are getting lost, will find them later...

--- src/lib/store/indexedDb.js
+++ src/lib/store/indexedDb.js
@@ -1,206 +1,125 @@
 define([
-  '../util', './pending', '../../vendor/IndexedDBShim'
-], function(util, pendingAdapter, _) {
+  '../util', './pending', './transaction'
+], function(util, pendingAdapter, transactionAdapter) {
 
-  var DB_NAME = 'remoteStorage';
-  var DB_VERSION = 1;
-  var OBJECT_STORE_NAME = 'nodes';
+  "use strict";
 
-  var logger = util.getLogger('store::indexed_db');
+  var DEFAULT_DB_NAME = "remotestorage";
+  var DB_VERSION = '1';
 
-  var adapter = function(indexedDB) {
-    if(! indexedDB) {
-      throw new Error("Not supported: indexedDB not found");
-    }
+  var logger = util.getLogger('store::indexedDb');
 
-    var DB = undefined;
+  var IndexedDBStore = function(indexedDB, dbName) {
+    this.idb = indexedDB;
+    this.dbName = dbName;
+    this._connect();
+  };
 
-    function removeDatabase() {
-      return util.getPromise(function(promise) {
-        if(DB) {
-          try {
-            DB.close();
-          } catch(exc) {
-            // ignored.
-          };
-          DB = undefined;
-        }
-        var request = indexedDB.deleteDatabase(DB_NAME);
+  var storeInterface = {
+    get: function(path) {
+      console.log('TRANS#get', path);
+      return this._transaction('get', path);
+    },
 
-        request.onsuccess = function() {
-          promise.fulfill();
-        };
+    set: function(path, node) {
+      console.log('TRANS#set', path);
+      return this._transaction('put', util.extend({ path: path }, node));
+    },
 
-        request.onerror = function() {
-          promise.reject('indexedDB.deleteDatabase failed!', request.error);
-        };
-      });
-    }
+    remove: function(path) {
+      console.log('TRANS#remove', path);
+      return this._transaction('delete', path);
+    },
 
-    function openDatabase() {
-      logger.info("Opening database " + DB_NAME + '@' + DB_VERSION);
-      return util.getPromise(function(promise) {
-        var dbRequest = indexedDB.open(DB_NAME, DB_VERSION);
-
-        function upgrade(db) {
-          logger.debug("Upgrade database: ", db);
-          db.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'key' });
-        }
-
-        dbRequest.onupgradeneeded = function(event) {
-          upgrade(event.target.result);
-        };
-        
-        dbRequest.onsuccess = function(event) {
-          logger.debug("DB REQUEST SUCCESS", event);
-          try {
-            var database = event.target.result;
-            if(typeof(database.setVersion) === 'function') {
-              logger.debug("setVersion supported");
-              if(database.version != DB_VERSION) {
-                var versionRequest = database.setVersion(DB_VERSION);
-                versionRequest.onsuccess = function(event) {
-                  logger.debug("VERSION REQUEST SUCCESS");
-                  upgrade(database);
-                  event.target.transaction.oncomplete = function() {
-                    promise.fulfill(database);
-                  };
-                };
-                versionRequest.onerror = function(event) {
-                  logger.error("Version request failed", event);
-                  promise.reject("Version request failed!");
-                };
-              } else {
-                promise.fulfill(database);
-              }
-            } else {
-              // assume onupgradeneeded is supported.
-              logger.debug("onupgradeneeded supported");
-              promise.fulfill(database);
-            }
-          } catch(exc) {
-            promise.reject(exc);
-          };
-        };
-
-        dbRequest.onerror = dbRequest.onfailure = function(event) {
-          logger.error("indexedDB.open failed: ", event);
-          promise.reject(new Error("Failed to open database!"));
-        }; 
-      });
-    }
-
-    function storeRequest(methodName) {
+    _transaction: function(methodName) {
       var args = Array.prototype.slice.call(arguments, 1);
-      return util.getPromise(function(promise) {
-        var store = DB.transaction(OBJECT_STORE_NAME, 'readwrite').
-          objectStore(OBJECT_STORE_NAME);
-        var request = store[methodName].apply(store, args);
-        request.onsuccess = function() {
-          promise.fulfill(request.result);
-        };
-        request.onerror = function(event) {
-          promise.reject(event.error);
-        };
+      var promise = util.getPromise();
+      var transaction = this.db.transaction(['nodes'], methodName === 'get' ? 'readonly' : 'readwrite');
+      var objectStore = transaction.objectStore('nodes');
+      console.log('objectStore is', objectStore);
+      var request = objectStore[methodName].apply(objectStore, args);
+      request.onsuccess = promise.fulfill;
+      request.onerror = promise.reject;
+      return promise;
+    }
+
+  };
+
+  IndexedDBStore.prototype = util.extend(pendingAdapter(), {
+
+    _connect: function() {
+      var dbRequest = this.idb.open(this.dbName, DB_VERSION);
+      dbRequest.onsuccess = dbRequest.onerror = util.bind(this._dbOpened, this);
+      dbRequest.onupgradeneeded = util.bind(this._upgradeDb, this);
+    },
+
+    _dbOpened: function(event) {
+      if(event.type === "error") {
+        console.error("Failed to open database '" + this.dbName + "': ", event);
+      } else {
+        this.db = event.target.result;
+        if(typeof(this.db) === "undefined") {
+          console.error("got no db", event);
+        }
+        console.log('got DB', this.db);
+
+        var store = util.extend({ db: this.db }, storeInterface);
+        var realAdapter = transactionAdapter(store, logger);
+        util.bindAll(realAdapter, this);
+        this.flush(realAdapter);
+        delete this.flush;
+        delete this.replaceWith;
+        delete this.get;
+        delete this.set;
+        delete this.remove;
+        delete this.forgetAll;
+        util.extend(this, realAdapter);
+      }
+    },
+
+    _upgradeDb: function(event) {
+      event.target.result.createObjectStore("nodes", {
+        keyPath: "path"
       });
     }
 
-    function wrapStore(store) {
-      function req(method) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        return util.getPromise(function(promise) {
-          var request = store[method].apply(store, args);
-          request.onsuccess = function() {
-            promise.fulfill(request.result);
-          };
-          request.onerror  = function(event) {
-            promise.reject(event.error);
-          };
-        });
-      }
-      return {
-        get: util.curry(req, 'get'),
-        set: function(key, value) {
-          value.key = key;
-          return req('put', value);
-        },
-        remove: util.curry(req, 'remove')
-      };
-    }
+  }, util.getEventEmitter('change'));
 
-    function makeTransaction(mode, body) {
-      return util.getPromise(function(promise) {
-        var transaction = DB.transaction(OBJECT_STORE_NAME, mode);
-        var store = transaction.objectStore(OBJECT_STORE_NAME);
-        transaction.oncomplete = promise.fulfill;
-        transaction.onerror = promise.reject;
-        body(wrapStore(store));
-      });
-    }
+  // var Transaction = function(objectStore) {
+  //   this.store = objectStore;
+  // };
 
-    var indexedDbStore = {
-      transaction: function(write, body) {
-        return makeTransaction(write ? 'readwrite' : 'readonly', body);
-      },
+  // Transaction.prototype = {
+  //   get: function(path) {
+  //     console.log("TRANS#get", path);
+  //     return this._store('get', path).then(function(event) {
+  //       return event.result;
+  //     });
+  //   },
 
-      on: function(eventName, handler) {
-        logger.debug("WARNING: indexedDB event handling not implemented");
-      },
-      // get: function(key) {
-      //   logger.debug("GET " + key);
-      //   return storeRequest('get', key);
-      // },
-      // set: function(key, value) {
-      //   logger.debug("SET " + key);
-      //   var node = value;
-      //   node.key = key;
-      //   return storeRequest('put', node);
-      // },
-      // remove: function(key) {
-      //   logger.debug("REMOVE " + key);
-      //   return storeRequest('delete', key);
-      // },
-      forgetAll: function() {
-        logger.debug("FORGET ALL");
-        return removeDatabase().then(doOpenDatabase);
-      }
-    };
+  //   set: function(path, node) {
+  //     console.log("TRANS#set", path);
+  //     return this._store('put', util.extend({ path: path }, node));
+  //   },
 
-    var tempStore = pendingAdapter();
+  //   remove: function(path) {
+  //     console.log("TRANS#remove", path);
+  //     return this._store('delete', path);
+  //   },
 
-    function replaceAdapter() {
-      if(tempStore.flush) {
-        tempStore.flush(indexedDbStore);
-        util.extend(tempStore, indexedDbStore);
-      }
-    }
+  //   _store: function(name) {
+  //     var args = Array.prototype.slice.call(arguments, 1);
+  //     var promise = util.getPromise();
+  //     var request = this.store[name].apply(this.store, args);
+  //     request.onsuccess = promise.fulfill;
+  //     request.onerror = promise.reject;
+  //     return promise;
+  //   }
+  // };
 
-    function doOpenDatabase() {
-      openDatabase().
-        then(function(db) {
-          logger.info("Database opened.");
-          DB = db;
-          replaceAdapter();
-        });
-    }
-
-    doOpenDatabase();
-
-    return tempStore;
+  return function(indexedDB, dbName) {
+    return new IndexedDBStore(indexedDB || (
+      (typeof(window) !== 'undefined' ? window : global).indexedDB
+    ), dbName || DEFAULT_DB_NAME);
   };
-
-  adapter.detect = function() {
-    var indexedDB;
-    if(typeof(window) !== 'undefined') {
-      // indexedDB = (window.indexedDB || window.webkitIndexedDB ||
-      //              window.mozIndexedDB || window.msIndexedDB);
-      if(window.webkitIndexedDB) {
-        window.shimIndexedDB.__useShim();
-      }
-      indexedDB = window.indexedDB;
-    }
-    return indexedDB;
-  };
-
-  return adapter;
 });
